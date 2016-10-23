b9b87636513724b0ae8175a58584fd6ac67ca3b4
James E. Pustejovsky <jepusto@gmail.com>
2016-10-22 21:15:24 -0500

    Simulating SE of Tau-U.

---
+++ simulations/Tau-U simulation.R
@@ -0,0 +1,49 @@
+library(tidyr)
+library(dplyr, warn.conflicts = FALSE)
+library(multidplyr)
+library(ggplot2)
+
+m <- 10
+n <- 10
+beta <- 0.05
+delta <- 0.5
+iterations <- 1000
+
+sample_TauU <- function(beta, delta, m, n, iterations) {
+  Tau_Us <- replicate(iterations, {
+    yA <- rnorm(m) + beta * (1:m)
+    yB <- rnorm(n, mean = delta) + beta * (m + 1:n)
+    Tau_U(yA, yB)
+  })
+  data.frame(sd = sd(Tau_Us))
+}
+
+cluster <- create_cluster(parallel::detectCores() - 1)
+set_default_cluster(cluster)
+cluster %>% 
+  cluster_library("SingleCaseES") %>%
+  cluster_assign_value("sample_TauU", sample_TauU)
+
+beta <- c(0, 0.05, 0.10)
+delta <- seq(0, 2, 0.1)
+m <- c(5, 10, 15, 20, 30)
+n <- c(5, 10, 15, 20, 30)
+
+params <- expand.grid(beta = beta, delta = delta, m = m, n = n)
+nrow(params)
+
+TauU_sim <- 
+  params %>%
+  group_by(beta, delta, m, n) %>%
+  partition(beta, delta, m, n) %>%
+  do(sample_TauU(beta = .$beta, delta = .$delta, m = .$m, n = .$n, iterations = 2000)) %>%
+  collect() %>%
+  mutate(se_null = sqrt((m + n + 1) / (3 * m * n))) %>% 
+  arrange(beta, delta, m , n)
+
+ggplot(TauU_sim, aes(delta, sd, color = factor(beta))) + 
+  geom_smooth(se = FALSE) + 
+  geom_hline(aes(yintercept = se_null), linetype = "dashed") + 
+  facet_grid(n ~ m, labeller = "label_both") + 
+  theme_bw() + theme(legend.position = "bottom") + 
+  labs(color = "Slope (per session)", x = "Treatment Effect", y = "SD(Tau-U)")
