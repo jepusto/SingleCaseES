ee530e29db1c5203c738cd9919bde18d1e9a8fd6
DMSwan <dms031000@gmail.com>
2018-01-17 14:43:19 -0600

    Phase_vals are necessary otherwise the interactive nature of shiny doesn't let me fill other example values

--- inst/shiny-examples/SCD-effect-sizes/mappings.R
+++ inst/shiny-examples/SCD-effect-sizes/mappings.R
@@ -1,6 +1,7 @@
 # Pre-loaded data info
 # path describes where to find the data
 # varnames are the Phase identifier variable name, Outcome variable name, and then any number of clustering variable names
+# phase vals are the values corresponding to the baseline and treatment phase, respectively
 # direction describes direction type
 # direction_var is the name of the variable that applies the direction.
 
@@ -8,9 +9,11 @@ example_list <- c("McKissick et al. (2010)" = "McKissick", "Schmidt (2007)" = "S
 
 exampleMapping <- list(
   Schmidt2007 = list(varnames = c("Condition", "Outcome", "Case_pseudonym", "Outcome_descriptor", "Phase_num"),
+                     phase_vals = c("A", "B"),
                      direction = "series",
                      direction_var = "direction"),
   McKissick = list(varnames =c("Condition", "Outcome", "Case_pseudonym"),
+                   phase_vals = c("A", "B"),
                    direction = "decrease")
   
 )
