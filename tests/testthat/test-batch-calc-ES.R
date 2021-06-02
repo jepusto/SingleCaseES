0c6dc7b25912e7602be04c577bd37e29678c6e49
Man Chen <mchen277@wisc.edu>
2021-06-01 22:33:08 -0500

    Added tests regarding aggregating options for the wide format and for confidence interval.

--- tests/testthat/test-batch-calc-ES.R
+++ tests/testthat/test-batch-calc-ES.R
@@ -7,10 +7,10 @@ test_that("batch_calc_ES() returns the same output
           as calc_ES() for a data frame with no grouping variable.", {
   
   data("McKissick")
-  dat1 <- McKissick %>% filter(Case_pseudonym == "Period 1")
+  dat1 <- McKissick %>% dplyr::filter(Case_pseudonym == "Period 1")
   
   # use calc_ES()
-  res_calc_ES <- calc_ES(condition = dat1$Condition, outcome = dat1$Outcome, baseline_phase = "A")
+  res_calc_ES <- calc_ES(condition = dat1$Condition, outcome = dat1$Outcome, baseline_phase = "A") %>% as_tibble()
   
   # use batch_calc_ES()
   res_batch_calc_ES <- batch_calc_ES(dat = dat1, condition = Condition, outcome = Outcome, baseline_phase = "A")
@@ -24,8 +24,8 @@ test_that("The aggregate argument works in batch_calc_ES().", {
   
   data("Schmidt2007")
   
-  # using 1/V
-  # calculate aggregate in two steps without specifying aggregating variables
+  # long format using 1/V
+  ## calculate aggregate in two steps by hand
   ES_ests <- 
     batch_calc_ES(dat = Schmidt2007,
                   grouping = c(Behavior_type, Case_pseudonym, Phase_num),
@@ -43,10 +43,10 @@ test_that("The aggregate argument works in batch_calc_ES().", {
     group_by(Behavior_type, Case_pseudonym, ES) %>% 
     mutate(ES_wt = 1 / SE^2) %>% 
     summarise(
-      ES_est = sum(Est * ES_wt) / sum(ES_wt),
-      ES_SE = sqrt(1 / sum(ES_wt)))
+      Est = sum(Est * ES_wt) / sum(ES_wt),
+      SE = sqrt(1 / sum(ES_wt)))
   
-  # calculate aggregate by specifying the aggregating variables
+  ## calculate aggregate by specifying the aggregating variables
   ES_agg <- 
     batch_calc_ES(dat = Schmidt2007,
                   grouping = c(Behavior_type, Case_pseudonym, Phase_num),
@@ -62,14 +62,55 @@ test_that("The aggregate argument works in batch_calc_ES().", {
                   format = "long")
   expect_identical(ES_agg_calculated, ES_agg)
   
-  # equal weights
+  # confidence interval
+  ES_SE_CI_agg <- 
+    batch_calc_ES(dat = Schmidt2007,
+                  grouping = c(Behavior_type, Case_pseudonym, Phase_num),
+                  aggregate = c(Behavior_type, Case_pseudonym),
+                  weighting = "1/V",
+                  condition = Condition,
+                  outcome = Outcome,
+                  ES = c("LRRi","LRRd", "Tau"),
+                  improvement = direction,
+                  scale = "count",
+                  bias_correct = TRUE,
+                  confidence = .95,
+                  format = "long")
+  expect_output(str(ES_SE_CI_agg), "tibble")
+  
+  # wide format using 1/V
+  ES_agg_wide <- 
+    batch_calc_ES(dat = Schmidt2007,
+                  grouping = c(Behavior_type, Case_pseudonym, Phase_num),
+                  aggregate = c(Behavior_type, Case_pseudonym),
+                  weighting = "1/V",
+                  condition = Condition,
+                  outcome = Outcome,
+                  ES = c("LRRi","LRRd", "Tau"),
+                  improvement = direction,
+                  scale = "count",
+                  bias_correct = TRUE,
+                  confidence = NULL,
+                  format = "wide")
+  
+  ES_agg_long_to_wide <- 
+    ES_agg %>% 
+    tidyr::pivot_wider(
+      names_from = ES,
+      names_glue = "{ES}_{.value}",
+      values_from = c(Est, SE)
+    )
+  
+  expect_identical(ES_agg_wide, ES_agg_long_to_wide)
+  
+  # long format using equal weights
   ES_agg_equal_calculated <- 
     ES_ests %>% 
     group_by(Behavior_type, Case_pseudonym, ES) %>% 
     mutate(ES_wt = 0.5) %>% # ABAB so use 0.5, better average within summarise()
     summarise(
-      ES_est = weighted.mean(Est, w = ES_wt),
-      ES_SE = sqrt(sum(SE^2 * ES_wt^2) / sum(ES_wt)^2))
+      Est = weighted.mean(Est, w = ES_wt),
+      SE = sqrt(sum(SE^2 * ES_wt^2) / sum(ES_wt)^2))
   
   ES_agg_equal <- 
     batch_calc_ES(dat = Schmidt2007,
