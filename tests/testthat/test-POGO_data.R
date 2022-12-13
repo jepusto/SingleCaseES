7feb7f368520745494efbca9690eb551cb79a311
jepusto <jepusto@gmail.com>
2022-12-13 14:03:05 -0600

    Added example of a unit test for POGO calculations.

--- tests/testthat/test-POGO_data.R
+++ tests/testthat/test-POGO_data.R
@@ -1,44 +1,52 @@
 library(dplyr)
 library(tidyr)
 
-## Test Crozier 2005 Calculations
-load("~/SingleCaseES/data/Crozier2005.Rdata")
+# Test Crozier 2005 Calculations
 
-Crozier2005_res <- as.data.frame(cbind(c("A1 to B1", "B1 to A2", "A2 to B2"), 
-                                       c(79.1, 64.2, 97.92))) %>%
-  rename(Phase_Shift = V1,
-         Article_ES = V2)
+test_that("POGO calculation agrees with article for Crozier 2005", {
+  
+  # We should replace the load() with the following
+  # data("Crozier2005", package = "SingleCaseES")
+  load("data/Crozier2005.Rdata")
+  
+  Crozier2005_res <- data.frame(Phase_Shift = c("A1 to B1", "B1 to A2", "A2 to B2"), 
+                                Article_ES = c(79.1, 64.2, 97.92))
+  
+  shift1 <- batch_calc_ES(dat = Crozier2005,
+                          condition = phase,
+                          baseline_phase = "A1",
+                          intervention_phase = "B1",
+                          outcome = score,
+                          ES = c("PoGO"),
+                          goal = 0)
+  
+  shift2 <- batch_calc_ES(dat = Crozier2005,
+                          condition = phase,
+                          baseline_phase = "B1",
+                          intervention_phase = "A2",
+                          outcome = score,
+                          ES = c("PoGO"),
+                          goal = 11.17)
+  
+  shift3 <- batch_calc_ES(dat = Crozier2005,
+                          condition = phase,
+                          baseline_phase = "A2",
+                          intervention_phase = "B2",
+                          outcome = score,
+                          ES = c("PoGO"),
+                          goal = 0)
+  
+  Crozier2005_PoGO <- rbind(shift1, shift2, shift3) 
+  
+  Crozier_Compare <- cbind(Crozier2005_res, Crozier2005_PoGO)
+  
+  expect_equal(round(Crozier_Compare$Article_ES,0), round(Crozier_Compare$Est, 0))
+  
+})
 
-shift1 <- batch_calc_ES(dat = Crozier2005,
-                        condition = phase,
-                        baseline_phase = "A1",
-                        intervention_phase = "B1",
-                        outcome = score,
-                        ES = c("PoGO"),
-                        goal = 0)
-
-shift2 <- batch_calc_ES(dat = Crozier2005,
-                        condition = phase,
-                        baseline_phase = "B1",
-                        intervention_phase = "A2",
-                        outcome = score,
-                        ES = c("PoGO"),
-                        goal = 11.17)
-
-shift3 <- batch_calc_ES(dat = Crozier2005,
-                        condition = phase,
-                        baseline_phase = "A2",
-                        intervention_phase = "B2",
-                        outcome = score,
-                        ES = c("PoGO"),
-                        goal = 0)
-
-Crozier2005_PoGO <- rbind(shift1, shift2, shift3) 
-
-Crozier_Compare <- cbind(Crozier2005_res, Crozier2005_PoGO)
 
 ## Test English 1997 Calculations
-load("~/SingleCaseES/data/English1997.Rdata")
+load("data/English1997.Rdata")
 
 English1997_res <- as.data.frame(cbind(c("Sue", "Don", "Jake", "Pete"),
                                        c(79.1, 69, 35.8, 108.2))) %>%
@@ -56,7 +64,7 @@ English_Compare <- left_join(English1997_res, English1997_PoGO, by = "case")
 
 
 ## Test Facon 2008 Calculations
-load("~/SingleCaseES/data/Facon2008.Rdata")
+load("data/Facon2008.Rdata")
 
 Facon2008_res <- as.data.frame(cbind(c("A to B", "B to C", "C to D", "D to E", "E to F", "F to G", "G to H", "H to I"),
                                      c(4.6, 12.4, 23.3, 43.5, 62.1, 77.9, 87.4, 106.2))) %>%
@@ -74,7 +82,7 @@ shift1_f <- batch_calc_ES(dat = Facon2008,
 shift2_f <- batch_calc_ES(dat = Facon2008,
                         condition = phase,
                         outcome = score,
-                        baseline_phase = "B",
+                        baseline_phase = "A",
                         intervention_phase = "C",
                         ES = c("PoGO"),
                         goal = 70)
@@ -82,7 +90,7 @@ shift2_f <- batch_calc_ES(dat = Facon2008,
 shift3_f <- batch_calc_ES(dat = Facon2008,
                         condition = phase,
                         outcome = score,
-                        baseline_phase = "C",
+                        baseline_phase = "A",
                         intervention_phase = "D",
                         ES = c("PoGO"),
                         goal = 70)
@@ -90,7 +98,7 @@ shift3_f <- batch_calc_ES(dat = Facon2008,
 shift4_f <- batch_calc_ES(dat = Facon2008,
                         condition = phase,
                         outcome = score,
-                        baseline_phase = "D",
+                        baseline_phase = "A",
                         intervention_phase = "E",
                         ES = c("PoGO"),
                         goal = 70)
@@ -98,7 +106,7 @@ shift4_f <- batch_calc_ES(dat = Facon2008,
 shift5_f <- batch_calc_ES(dat = Facon2008,
                         condition = phase,
                         outcome = score,
-                        baseline_phase = "E",
+                        baseline_phase = "A",
                         intervention_phase = "F",
                         ES = c("PoGO"),
                         goal = 70)
@@ -106,7 +114,7 @@ shift5_f <- batch_calc_ES(dat = Facon2008,
 shift6_f <- batch_calc_ES(dat = Facon2008,
                         condition = phase,
                         outcome = score,
-                        baseline_phase = "F",
+                        baseline_phase = "A",
                         intervention_phase = "G",
                         ES = c("PoGO"),
                         goal = 70)
@@ -114,7 +122,7 @@ shift6_f <- batch_calc_ES(dat = Facon2008,
 shift7_f <- batch_calc_ES(dat = Facon2008,
                         condition = phase,
                         outcome = score,
-                        baseline_phase = "G",
+                        baseline_phase = "A",
                         intervention_phase = "H",
                         ES = c("PoGO"),
                         goal = 70)
@@ -122,7 +130,7 @@ shift7_f <- batch_calc_ES(dat = Facon2008,
 shift8_f <- batch_calc_ES(dat = Facon2008,
                         condition = phase,
                         outcome = score,
-                        baseline_phase = "H",
+                        baseline_phase = "A",
                         intervention_phase = "I",
                         ES = c("PoGO"),
                         goal = 70)
@@ -132,7 +140,7 @@ Facon2008_PoGO <- rbind(shift1_f, shift2_f, shift3_f, shift4_f, shift5_f, shift6
 Facon_Compare <- cbind(Facon2008_res, Facon2008_PoGO)
 
 ## Test Olszewski 2017 Calculations
-load("~/SingleCaseES/data/Olszewski2017.RData")
+load("data/Olszewski2017.RData")
 
 Olszewski2017_res <- as.data.frame(cbind(c("Blends", "Segmenting", "First Part ID", "First Sound ID"),
                                          c(23.0, 85.3, 100, 26))) %>%
